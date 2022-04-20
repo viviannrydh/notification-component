5e5053b95ced6431893b2f63e79dd8195d9dc068
Viviann Rydh <rydh.vivian@gmail.com>
2022-04-20 11:39:45 +0200

    setup repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+
+import react, { useState } from 'react';
+import './App.css'
+import { motion } from 'framer-motion';
+
 
 function App() {
+  const [active, setActive] = useState(false);
+  const [activeTwo, setActiveTwo] = useState(false);
+  const [activeThree, setActiveThree] = useState(false);
+
+  const toggleShake = () => { 
+    setActive(!active)
+  }
+  const toggleScale = () => {
+    setActiveTwo(!activeTwo)
+  }
+
+  const toggleBounce = () => { 
+    setActiveThree(!activeThree)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <div
+        className=
+        {active
+          ? 'notification-shake'
+          : "notification-text"}>
+        Youth is not a time of life;
+        it is a state of mind;
+        it is not a matter of rosy cheeks,
+        red lips and supple knees;
+        it is a matter of the will,
+        a quality of the imagination,
+        a vigor of the emotions;
+        it is the freshness of the deep springs of life.
+      </div>
+      <button onClick={() => toggleShake()}>Shake it</button>
+      <div
+        className=
+        {activeTwo
+          ? 'notification-scale'
+          : "notification-middle"}>
+        Youth is not a time of life;
+        it is a state of mind;
+        it is not a matter of rosy cheeks,
+        red lips and supple knees;
+        it is a matter of the will,
+        a quality of the imagination,
+        a vigor of the emotions;
+        it is the freshness of the deep springs of life.
+      </div>
+      <button onClick={() => toggleScale()}>Transform it</button>
+    
+    <div
+        className=
+        {activeThree
+          ? 'notification-bounce'
+          : "notification-top"}>
+      Youth is not a time of life;
+      it is a state of mind;
+      it is not a matter of rosy cheeks,
+      red lips and supple knees;
+      it is a matter of the will,
+      a quality of the imagination,
+      a vigor of the emotions;
+      it is the freshness of the deep springs of life.
+      </div>
+      <button onClick={() => toggleBounce()}>Bounce it</button>
+    
     </div>
+     
   );
 }
 
